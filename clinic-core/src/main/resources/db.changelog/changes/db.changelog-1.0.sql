cf36d40c4ca2841d3ca4d38a29213717c2a9c57a
Danila <nonier1108@gmail.com>
2022-11-29 15:08:04 +0300

    add migration scripts

---
+++ clinic-core/src/main/resources/db.changelog/changes/db.changelog-1.0.sql
@@ -0,0 +1,49 @@
+--liquibase formatted sql
+
+--changeset dilyin:1
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  username VARCHAR(128) UNIQUE NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  name VARCHAR(128),
+  surname VARCHAR(128)
+);
+--rollback DROP TABLE users;
+
+--changeset dilyin:2
+CREATE TABLE doctor(
+    id BIGSERIAL PRIMARY KEY,
+    age_group VARCHAR(128) NOT NULL,
+    work_experience INTEGER,
+    rank VARCHAR(128) NOT NULL,
+    user_id BIGINT REFERENCES users(id)
+);
+--rollback DROP TABLE doctor;
+
+--changeset dilyin:3
+CREATE TABLE role(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) UNIQUE NOT NULL
+);
+--rollback DROP TABLE role;
+
+--changeset dilyin:4
+CREATE TABLE user_roles(
+    user_id BIGINT REFERENCES users(id),
+    role_id BIGINT REFERENCES role(id)
+);
+--rollback DROP TABLE user_roles;
+
+--changeset dilyin:5
+CREATE TABLE specialization(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(128) UNIQUE NOT NULL
+);
+--rollback DROP TABLE specialization;
+
+--changeset dilyin:6
+CREATE TABLE doctor_specialization(
+    doctor_id BIGINT REFERENCES doctor(id),
+    specialization_id BIGINT REFERENCES specialization(id)
+);
+--rollback DROP TABLE doctor_specialization;
