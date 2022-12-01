b33392d7d7af68399ef3c700ea5fc452bf19573c
Danila <nonier1108@gmail.com>
2022-12-01 15:29:44 +0300

    add simple ControllerAdvice

---
+++ clinic-core/src/main/java/com/nonier/cliniccore/controller/advice/ControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.nonier.cliniccore.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.EntityNotFoundException;
+
+@Component
+@RestControllerAdvice(basePackages = "com.nonier.cliniccore.controller")
+public class ControllerAdvice {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handle(EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handle(RuntimeException exception) {
+        return ResponseEntity.internalServerError()
+                .body("Exception occured: %s cause: %s"
+                        .formatted(exception.getClass(), exception.getMessage()));
+    }
+}
