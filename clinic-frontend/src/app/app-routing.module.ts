4271ee83ed91a9ae8eaa71c149a1520dcd323bbd
Danila <nonier1108@gmail.com>
2023-04-09 20:17:53 +0300

    add routing module

---
+++ clinic-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
+import {RegistrationPageComponent} from "./registration-page/registration-page.component";
+import {DoctorsPageComponent} from "./doctors-page/doctors-page.component";
+
+const routes: Routes = [
+  {
+    path: 'auth', component: AuthLayoutComponent, children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'registration', component: RegistrationPageComponent}
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, children: [
+      {path: '', redirectTo: '/doctors', pathMatch: 'full'},
+      {path: 'doctors', component: DoctorsPageComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+  }
+)
+export class AppRoutingModule {
+
+}
--- clinic-frontend/src/app/app.module.ts
+++ clinic-frontend/src/app/app.module.ts
@@ -2,27 +2,35 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
 import {DoctorsComponent} from "./views/doctors/doctors.component";
-import {NavComponent} from "./views/header/nav/nav.component";
 import {SearchComponent} from "./views/search/search.component";
 import {FreeConsultationPipe} from "./pipe/free-consultation.pipe";
-import {LoginComponent} from "./views/login/login.component";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {NgxBootstrapMultiselectModule} from "ngx-bootstrap-multiselect";
+import {AppRoutingModule} from "./app-routing.module";
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { DoctorsPageComponent } from './doctors-page/doctors-page.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         DoctorsComponent,
-        NavComponent,
         SearchComponent,
         FreeConsultationPipe,
-        LoginComponent
+        LoginPageComponent,
+        AuthLayoutComponent,
+        SiteLayoutComponent,
+        RegistrationPageComponent,
+        DoctorsPageComponent
     ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
     FormsModule,
     NgxBootstrapMultiselectModule
   ],
